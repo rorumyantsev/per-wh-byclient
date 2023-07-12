3f34fe74020a7dd274a85e88e3901f6813b0a479
rorumyantsev <126847670+rorumyantsev@users.noreply.github.com>
2023-07-12 16:07:03 -0500

    Update wh_report_per.py

--- wh_report_per.py
+++ wh_report_per.py
@@ -105,8 +105,8 @@ def get_report(client, option="Today", start_=None, end_=None) -> pandas.DataFra
 
     today = today.strftime("%Y-%m-%d")
     report = []
-    st.write(client)
-    st.write(CLIENT_MAP["Oechsle "])
+    #st.write(client)
+    #st.write(CLIENT_MAP["Oechsle "])
     
     
     client_num = CLIENT_MAP[client]
